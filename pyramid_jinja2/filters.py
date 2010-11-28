8a23d0a47e4bb92a39584ee75c085731dfd2d40d
Atsushi Odagiri <aodagx@gmail.com>
2010-11-28 20:03:13 +0900

    jinja2 filters for url.

---
+++ pyramid_jinja2/filters.py
@@ -0,0 +1,13 @@
+from pyramid.url import model_url, route_url
+from pyramid.threadlocal import get_current_request
+
+__all__ = ['model_url_filter',
+        ]
+def model_url_filter(model, *elements):
+    request = get_current_request()
+    return model_url(model, request, *elements)
+
+def route_url_filter(route_name, *elements, **kw):
+    request = get_current_request()
+    return route_url(route_name, request, *elements, **kw)
+
