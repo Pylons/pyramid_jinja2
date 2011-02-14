de2d5c8f5506c9327d85fe842764442a5f19f840
Rocky Burt <rocky@serverzen.com>
2011-02-14 10:02:40 -0330

    Updated paster template to more closely resemble pyramid trunk paster templates - fixes issue 15

--- pyramid_jinja2/paster.py
+++ pyramid_jinja2/paster.py
@@ -1,8 +1,8 @@
-from paste.script.templates import Template
 from paste.util.template import paste_script_template_renderer
+from pyramid import paster
 
 
-class Jinja2ProjectTemplate(Template):
+class Jinja2ProjectTemplate(paster.PyramidTemplate):
     _template_dir = 'paster_template'
     summary = 'pyramid jinja2 starter project'
     template_renderer = staticmethod(paste_script_template_renderer)
