dc1556401be29ac7922158d401eca30e975862f4
Domen Kožar <domen@dev.si>
2012-09-14 12:55:13 +0200

    use sphinx.ext.intersphinx to link Jinja2 docs

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ if 'sphinx-build' in ' '.join(sys.argv): # protect against dumb importers
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
