9f0ec717bf667139434e453f82869ed6d4478e9b
Michael R <michr@example.com>
2012-11-15 01:03:34 -0800

    updated tests based on mcdonc review

--- pyramid_jinja2/tests/test_settings.py
+++ pyramid_jinja2/tests/test_settings.py
@@ -4,15 +4,41 @@
 Tests for jinja2 settings.
 """
 
-import unittest
-from pyramid_jinja2.tests.base import Base, DummyRendererInfo
-from pyramid_jinja2 import renderer_factory
+from unittest import TestCase
+from pyramid import testing
 
 
-class Test_settings(Base, unittest.TestCase):
+class DummyRendererInfo(object):
+    """
+    RendererInfo fixture.
+    """
+    def __init__(self, kw):
+        self.__dict__.update(kw)
+        if 'registry' in self.__dict__:
+            self.settings = self.registry.settings
+
+
+class Test_settings(TestCase):
+    """
+    Test case for settings.
+    """
+
+    def setUp(self):
+        self.request = testing.DummyRequest()
+        self.config = testing.setUp(request=self.request)
+        self.request.registry = self.config.registry
+
+    def tearDown(self):
+        testing.tearDown()
+        del self.config
+
+    def _callFUT(self, info):
+        from pyramid_jinja2 import renderer_factory
+        return renderer_factory(info)
 
     def test_settings(self):
         from pyramid_jinja2 import IJinja2Environment
+        from pyramid_jinja2.tests.base import DummyRendererInfo
         self.config.registry.settings.update(
             {'jinja2.block_start_string': '<<<',
              'jinja2.block_end_string': '>>>',
@@ -36,7 +62,7 @@ class Test_settings(Base, unittest.TestCase):
              })
 
         # call renderer so the Jinja2 environment is created
-        renderer_factory(info)
+        self._callFUT(info)
         environ = self.config.registry.getUtility(IJinja2Environment)
 
         self.assertEqual(environ.block_start_string, '<<<')
