d41f8648325a2d7188746f80878fe8323b5257a5
Mario Idival <marioidival@gmail.com>
2015-03-02 13:04:03 -0300

    remove get_current_request from filters

--- pyramid_jinja2/filters.py
+++ pyramid_jinja2/filters.py
@@ -1,6 +1,5 @@
 from pyramid.url import resource_url, route_url, static_url
 from pyramid.url import route_path, static_path
-from pyramid.threadlocal import get_current_request
 from jinja2 import contextfilter
 
 
@@ -18,7 +17,7 @@ def model_url_filter(ctx, model, *elements, **kw):
     """A filter from ``model`` to a string representing the absolute URL.
     This filter calls :py:func:`pyramid.url.resource_url`.
     """
-    request = ctx.get('request') or get_current_request()
+    request = ctx.get('request')
     return resource_url(model, request, *elements, **kw)
 
 
@@ -27,7 +26,7 @@ def model_path_filter(ctx, model, *elements, **kw):
     """A filter from ``model`` to a string representing the relative URL.
     This filter calls :py:meth:`pyramid.request.Request.resource_path`.
     """
-    request = ctx.get('request') or get_current_request()
+    request = ctx.get('request')
     return request.resource_path(model, *elements, **kw)
 
 
@@ -36,7 +35,7 @@ def route_url_filter(ctx, route_name, *elements, **kw):
     """A filter from ``route_name`` to a string representing the absolute URL.
     This filter calls :py:func:`pyramid.url.route_url`.
     """
-    request = ctx.get('request') or get_current_request()
+    request = ctx.get('request')
     return route_url(route_name, request, *elements, **kw)
 
 
@@ -45,7 +44,7 @@ def route_path_filter(ctx, route_name, *elements, **kw):
     """A filter from ``route_name`` to a string representing the relative URL.
     This filter calls :py:func:`pyramid.url.route_path`.
     """
-    request = ctx.get('request') or get_current_request()
+    request = ctx.get('request')
     return route_path(route_name, request, *elements, **kw)
 
 
@@ -54,7 +53,7 @@ def static_url_filter(ctx, path, **kw):
     """A filter from ``path`` to a string representing the absolute URL.
     This filter calls :py:func:`pyramid.url.static_url`.
     """
-    request = ctx.get('request') or get_current_request()
+    request = ctx.get('request')
     return static_url(path, request, **kw)
 
 
@@ -63,5 +62,5 @@ def static_path_filter(ctx, path, **kw):
     """A filter from ``path`` to a string representing the relative URL.
     This filter calls :py:func:`pyramid.url.static_path`.
     """
-    request = ctx.get('request') or get_current_request()
+    request = ctx.get('request')
     return static_path(path, request, **kw)
