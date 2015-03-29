b5ee98f22b99b76c0e324b1663821eec2cffa2d4
Marshall <marshallthornton@gmail.com>
2015-03-29 19:25:07 -0400

    Adjusted loader to not throw OS Error 22 due to passing a <module>:<path> URI to the FileSystemLoader

--- pyramid_jinja2/__init__.py
+++ pyramid_jinja2/__init__.py
@@ -232,14 +232,12 @@ class SmartAssetSpecLoader(FileSystemLoader):
                         return src
 
         # try to load the template from the default search path
-        for parent in rel_searchpath:
-            try:
-                uri = os.path.join(parent, template)
-                # avoid recursive includes
-                if uri not in rel_chain:
-                    return FileSystemLoader.get_source(self, environment, uri)
-            except TemplateNotFound:
-                pass
+        try:
+            # avoid recursive includes
+            if template not in rel_chain:
+                return FileSystemLoader.get_source(self, environment, template)
+        except TemplateNotFound:
+            pass
 
         # we're here because of an exception during the last step so extend
         # the message and raise an appropriate error
